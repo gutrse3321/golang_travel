6ef6933d62908a896531644580a70172ee56da20
tomo <gutrse3321@live.com>
2018-09-29 19:05:47 +0800

    form

--- web/less1.go
+++ web/less1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -10,6 +11,7 @@ import (
 func main()  {
 	// 设置访问的路由
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
 	// 设置监听的端口
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
@@ -36,4 +38,24 @@ func sayhelloName(writer http.ResponseWriter, request *http.Request) {
 	// 输出到页面的
 	fmt.Fprintf(writer, "hello Tomo")
 	fmt.Print("\n")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		// GET请求页面
+		t, _ := template.ParseFiles("web/login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		// 请求的是登陆数据，那么执行登陆的逻辑判断
+		r.ParseForm()
+		//fmt.Println("username: ", r.Form["username"])
+		//fmt.Println("password: ", r.Form["password"])
+		//转义
+		//输出到服务端
+		fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password: ", template.HTMLEscapeString(r.Form.Get("password")))
+		// 输出到页面的
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
 }
