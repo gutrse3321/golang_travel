237ed5dc1fa7af145fafa166cd7fc59b2cd9c385
tomo <gutrse3321@live.com>
2018-10-15 19:12:19 +0800

    [Code] struct存值结构体，chromedp抓动态数据

--- reptile/colly.go
+++ reptile/colly.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/runner"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/proxy"
 	"log"
@@ -10,31 +13,37 @@ import (
 )
 
 func main() {
-	if choeeiBaseData() {
-		log.Fatalln("ok")
+	var c = colly.NewCollector(
+		colly.AllowedDomains("www.pornhub.com"),
+		colly.Async(true),
+	)
+
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+	rp, err := proxy.RoundRobinProxySwitcher("http://127.0.0.1:1080", "https://127.0.0.1:1080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	c.SetProxyFunc(rp)
+
+	flag, baseBox := choeeiBaseData(c)
+	if !flag {
+		log.Fatalln("Fatal Fail!")
+	}
+
+	for _, item := range baseBox {
+		tempVar := item.(map[string]interface{})
+		videoDetailPage(tempVar["linkUrl"].(string))
 	}
 }
 
-func choeeiBaseData() (flag bool) {
+func choeeiBaseData(c *colly.Collector) (flag bool, baseBox []interface{}) {
 	var count int64 = 0
 	for {
 		count++
-		if count > 72 {
-			return true
+		if count > 1 {
+			return true, baseBox
 		}
 
-		var c = colly.NewCollector(
-			colly.AllowedDomains("www.pornhub.com"),
-			colly.Async(true),
-		)
-
-		c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-		rp, err := proxy.RoundRobinProxySwitcher("http://127.0.0.1:1080", "https://127.0.0.1:1080")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		c.SetProxyFunc(rp)
-		//c.Async = true
 		c.OnHTML("a.img", func(e *colly.HTMLElement) {
 			link := e.Attr("href")
 			title := e.Attr("title")
@@ -43,6 +52,11 @@ func choeeiBaseData() (flag bool) {
 			fmt.Printf("Video found: %q, Link: %s\n", title, link)
 			//fmt.Printf("Link Video: %s\n", videoUrl)
 			fmt.Printf("Link image: %s\n\n", imgSrc)
+			avbox := make(map[string]interface{})
+			avbox["title"] = title
+			avbox["imgUrl"] = imgSrc
+			avbox["linkUrl"] = link
+			baseBox = append(baseBox, avbox)
 		})
 
 		c.OnRequest(func(r *colly.Request) {
@@ -59,14 +73,55 @@ func choeeiBaseData() (flag bool) {
 	}
 }
 
-//func videoDetailPage(url string, ctx *colly.Collector) (videoUrl string) {
-//	ctx.OnHTML("head", func(e *colly.HTMLElement) {
-//		videoUrl = e.ChildAttr("meta[name='twitter:player']", "content")
-//		//source, exist := e.DOM.Children().Find("video").Children().Find("source").Attr("src")
-//		//if !exist {
-//		//	source = "视频不存在"
-//		//}
-//	})
-//	ctx.Visit(url)
-//	return
-//}
+func videoDetailPage(url string) {
+	var buffer bytes.Buffer
+	//https://www.pornhub.com/view_video.php?viewkey=ph5a34470258c5e
+	buffer.WriteString("https://www.pornhub.com")
+	buffer.WriteString(url)
+
+	var err error
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	run, err := runner.New(runner.Flag("headless", true))
+	runner.URL(buffer.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = run.Start(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, err := chromedp.New(ctxt, chromedp.WithRunner(run))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var attr string
+	var ok bool
+	err = c.Run(ctxt, getVideoRealPath(buffer.String(), &attr, &ok))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("attr: ", attr)
+
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getVideoRealPath(s string, attr *string, ok *bool) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(s),
+		chromedp.WaitReady(`source`),
+		chromedp.AttributeValue(`source`, "src", attr, ok, chromedp.ByQuery),
+	}
+}
